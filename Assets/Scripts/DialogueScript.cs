fc9bc446eb345e8b7241b23df56cbc04c558a0e8
gregglrnt <68611230+gregglrnt@users.noreply.github.com>
2023-03-28 19:04:49 +0200

    First push

---
+++ Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DialogueScript : MonoBehaviour
+{
+    void Start() {
+
+    }   
+
+    void Update() {
+
+    } 
+}
+
+
+public class SMS {
+    public string sender;
+}
